82257ed9e61576cd2ad8b5990540572165a9c772
Aqibbutt20 <aqib@outliant.com>
2023-01-11 04:22:57 -0800

    Fixed the setactive tab feature and styling

--- src/common/components/sections/Tabs.tsx
+++ src/common/components/sections/Tabs.tsx
@@ -5,8 +5,10 @@ import React, { useEffect, useState } from 'react'
 const TabsSection = ({ section }: { section: Type_Tabs }) => {
     const { items } = section.fields;
     const [selectedTab, setselectedTab] = useState<any>(items[0])
+    const [isActive, setIsActive] = useState(null);
     const handleClick = (index: number) => {
         const data = items[index]
+        setIsActive(index);
         setselectedTab(data)
     }
     
@@ -17,15 +19,15 @@ const TabsSection = ({ section }: { section: Type_Tabs }) => {
                     <tr>
                         {/* <td onClick={() => handleClick("10")} className="active" data-year="10">10 Years</td> */}
                         {items.map((value, index) => (
-                            <td onClick={() => handleClick(index)} data-year="30">{value.fields.header}</td>
+                            <td className={`${isActive == index && 'active'}`} onClick={() => handleClick(index)} data-year="30">{value.fields.header}</td>
                         ))}
                     </tr>
                 </tbody>
             </table>
-            <div>
+            <div className='head-title'>
                 {selectedTab.fields.title}
             </div>
-            <RichTextRenderer text={selectedTab.fields.content.fields.content} />
+            <RichTextRenderer text={selectedTab.fields.content?.fields.content} />
         </>
     )
 }
