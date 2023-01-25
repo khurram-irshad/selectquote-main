74f81d2e6b35b28bf353706c7514e21bfdb9e57c
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2023-01-26 01:05:19 +0800

    chore: make sub navigation show independently

--- src/common/components/rich-text/RichTextRenderer.tsx
+++ src/common/components/rich-text/RichTextRenderer.tsx
@@ -7,14 +7,7 @@ import { EmbeddedAsset } from "./EmbeddedAsset";
 const PlainHyperlink = (props) => <Hyperlink {...props} type="PlainLink" />;
 const AssetHyperlink = (props) => <Hyperlink {...props} type="AssetLink" />;
 
-const RichTextRenderer = ({
-  text,
-  color,
-}: {
-  text: any;
-  color?: string;
-}) => {
-
+const RichTextRenderer = ({ text, color }: { text: any; color?: string }) => {
   return (
     <>
       {documentToReactComponents(text, {
@@ -23,10 +16,19 @@ const RichTextRenderer = ({
           [INLINES.ASSET_HYPERLINK]: AssetHyperlink,
           [INLINES.ENTRY_HYPERLINK]: () => null, // Ignore entry hyperlink
           [BLOCKS.EMBEDDED_ASSET]: EmbeddedAsset,
-          [BLOCKS.PARAGRAPH]: (node, children) => <Text color={color}>{children}</Text>
+          [BLOCKS.PARAGRAPH]: (node, children) => (
+            <Text color={color}>{children}</Text>
+          ),
         },
-        renderText: text =>
-          text.split("\n").flatMap((text, i) => [i > 0 && <div style={{ paddingBottom: '10px' }}></div>, text])
+        renderText: (text) =>
+          text
+            .split("\n")
+            .flatMap((text, i) => [
+              i > 0 && (
+                <div key={i.toString()} style={{ paddingBottom: "10px" }}></div>
+              ),
+              text,
+            ]),
       })}
     </>
   );
