eb458edaf1b4a64de98b34b84d4323b567722337
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2023-01-31 01:27:43 +0800

    chore: add video on career page

--- src/common/components/sections/Video.tsx
+++ src/common/components/sections/Video.tsx
@@ -1,12 +1,33 @@
-import { Type_Video } from '@common/types/Type_Video'
-import React from 'react'
+import { Type_Video } from "@common/types/Type_Video";
+import RichTextRenderer from "@components/rich-text/RichTextRenderer";
+import React from "react";
+import ReactPlayer from "react-player";
 
 const VideoSection = ({ section }: { section: Type_Video }) => {
-    return (
-        <div>
-            <h1>Video section here....</h1>
+  const { title, video, body, footer } = section.fields;
+  console.log(video);
+  return (
+    <div className="video-section">
+      <div className="wrapper">
+        <div className="video">
+          <ReactPlayer
+            playing
+            controls
+            width="217px"
+            height="250px"
+            url={`${video.fields.file.url}`}
+          />
         </div>
-    )
-}
+        <div className="copy">
+          <p className="title">{title}</p>
+          <h5 className="body">{body}</h5>
+          <div className="footer-wrapper">
+            <p className="footer">{footer}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default VideoSection
+export default VideoSection;
