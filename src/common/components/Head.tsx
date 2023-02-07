7d2a45538974a2729502985e5d1a50d3195ac30e
Rashid Aslam <rashid@outliant.com>
2023-02-07 22:39:35 +0500

    feat: imported poppins

--- src/common/components/Head.tsx
+++ src/common/components/Head.tsx
@@ -56,6 +56,12 @@ const PageHead = ({ page }: { page: Type_Page }) => {
       <meta name="twitter:label1" content="Est. reading time" />
       <meta name="twitter:data1" content="9 minutes" />
       <meta property="og:image" content={pageImage} />
+
+
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
+     
       <meta name="keywords" content={pageKeywords} />
       {pageCanonicalUrl !== "" && (
         <link rel="canonical" href={pageCanonicalUrl} />
