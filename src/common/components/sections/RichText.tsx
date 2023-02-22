fd13011b8b1359da6df014980d2413f7d73ef0ce
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2023-02-22 23:29:27 +1300

    chore: fix styling and colors

--- src/common/components/sections/RichText.tsx
+++ src/common/components/sections/RichText.tsx
@@ -4,27 +4,31 @@ import RichTextRenderer from "@components/rich-text/RichTextRenderer";
 import React from "react";
 
 const RichTextSection = ({ section }: { section: Type_RichTextCustom }) => {
-  const {
-    content,
-    devices,
-    contentId
-  } = section.fields;
+  const { content, devices, contentId } = section.fields;
 
-  const desktop = devices?.find(item => item.fields?.type === DeviceType.Desktop);
-  const mobile = devices?.find(item => item.fields?.type === DeviceType.Mobile);
+  const desktop = devices?.find(
+    (item) => item.fields?.type === DeviceType.Desktop
+  );
+  const mobile = devices?.find(
+    (item) => item.fields?.type === DeviceType.Mobile
+  );
 
   return (
     <>
-      <section
-        className={`richtext-render `}
-
-      >
-        <div className={`wp-container-desktop text-${desktop?.fields?.textAlign}`} style={{ background: desktop?.fields?.backgroundColor }}>
+      <section className={`richtext-render `}>
+        <div
+          className={`wp-container-desktop text-${desktop?.fields?.textAlign}`}
+          style={{ background: desktop?.fields?.backgroundColor }}
+        >
           <div
             id={contentId}
-            className={desktop?.fields?.fullWidth ? "container wp-container" : ""}
+            className={
+              desktop?.fields?.fullWidth ? "container wp-container" : ""
+            }
             style={{
-              width: `${desktop?.fields?.width}`,
+              width: `${
+                desktop?.fields?.widthPercentage ?? desktop?.fields?.width
+              }`,
               padding: `${desktop?.fields?.padding}`,
               fontWeight: `${desktop?.fields?.fontWeight}`,
               lineHeight: `${desktop?.fields?.lineHeight}`,
@@ -32,7 +36,7 @@ const RichTextSection = ({ section }: { section: Type_RichTextCustom }) => {
               color: `${desktop?.fields?.textColor} !important`,
               letterSpacing: `${desktop?.fields?.letterSpacing} !important`,
               fontFamily: `${desktop?.fields?.fontFamily}`,
-              margin: desktop?.fields?.margin
+              margin: desktop?.fields?.margin,
             }}
           >
             <RichTextRenderer
@@ -41,10 +45,15 @@ const RichTextSection = ({ section }: { section: Type_RichTextCustom }) => {
             />
           </div>
         </div>
-        <div className={`wp-container-mobile text-${mobile?.fields?.textAlign}`} style={{ background: mobile?.fields?.backgroundColor }}>
+        <div
+          className={`wp-container-mobile text-${mobile?.fields?.textAlign}`}
+          style={{ background: mobile?.fields?.backgroundColor }}
+        >
           <div
             id={contentId}
-            className={mobile?.fields?.fullWidth ? "container wp-container" : ""}
+            className={
+              mobile?.fields?.fullWidth ? "container wp-container" : ""
+            }
             style={{
               width: `${mobile?.fields?.width}`,
               padding: `${mobile?.fields?.padding}`,
@@ -54,10 +63,13 @@ const RichTextSection = ({ section }: { section: Type_RichTextCustom }) => {
               color: `${mobile?.fields?.textColor} !important`,
               letterSpacing: `${mobile?.fields?.letterSpacing} !important`,
               fontFamily: `${mobile?.fields?.fontFamily}`,
-              margin: mobile?.fields?.margin
+              margin: mobile?.fields?.margin,
             }}
           >
-            <RichTextRenderer text={content} color={mobile?.fields?.textColor} />
+            <RichTextRenderer
+              text={content}
+              color={mobile?.fields?.textColor}
+            />
           </div>
         </div>
       </section>
