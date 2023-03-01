72a0856869251cd08a0f4e34537dc35fa80a37c7
Rashid Aslam <rashid@outliant.com>
2023-03-01 21:10:33 +0500

    feat: partnership page split

--- src/common/types/Type_Form.ts
+++ src/common/types/Type_Form.ts
@@ -3,6 +3,7 @@ import { Type_RichTextCustom } from "./Type_RichText";
 
 interface Form {
   title: Type_RichTextCustom;
+  step:  Contentful.EntryFields.Symbol;
 }
 
 export type Type_Form = Contentful.Entry<Form>;
