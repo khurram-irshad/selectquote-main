9c44671402ab79650814c81775a415e3532b4a0a
Rashid Aslam <rashid@outliant.com>
2023-03-03 23:27:18 +0500

    fix: import issue fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Layout from "@components/layout";
 import { Type_Page } from "@common/types/Type_Page";
 import {
-  ComponentContentTypes,
   PageContentTypes,
 } from "@common/constants/app.constant";
 import _ from "lodash";
@@ -12,25 +11,6 @@ import { getEntry } from "@common/services/api";
 
 export default ({ page }: { page: Type_Page }) => {
   const { header, sections } = page?.fields;
-
-  useEffect(() => {
-    // const reviewSection = sections.find((section) => {
-    //   const contentTypeId = _.get(section, "sys.contentType.sys.id");
-    //   return contentTypeId === ComponentContentTypes.CustomerReview;
-    // });
-    // if (reviewSection) {
-    //   var aScript = document.createElement("script");
-    //   aScript.type = "text/javascript";
-    //   aScript.src =
-    //     "//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js";
-    //   aScript.async = true;
-    //   document.head.appendChild(aScript);
-    //   aScript.onload = function () {
-    //     var trustbox = document.getElementById("trustbox");
-    //     window["Trustpilot"].loadFromElement(trustbox, true);
-    //   };
-    // }
-  }, []);
 
   return (
     <Layout page={page} header={header} >
