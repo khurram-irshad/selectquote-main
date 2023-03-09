558af5b4f7c4895743a4a06208347ab7b162ad4d
Rashid Aslam <rashid@outliant.com>
2023-03-09 17:11:43 +0500

    feat: added settimout

--- src/common/components/Head.tsx
+++ src/common/components/Head.tsx
@@ -67,30 +67,32 @@ const PageHead = ({ page }: { page: Type_Page }) => {
 
 
   useEffect(() => {
-    injectedScript.map(inject => { 
-      let createdElement: any = document.createElement(inject.element);
-      createdElement.type = inject.type;
-      createdElement.src = inject.source;
-      createdElement.async = inject.async;
-     return document.head.appendChild(createdElement)
-    })
+    setTimeout(() => {
+      injectedScript.map(inject => {
+        let createdElement: any = document.createElement(inject.element);
+        createdElement.type = inject.type;
+        createdElement.src = inject.source;
+        createdElement.async = inject.async;
+        return document.head.appendChild(createdElement)
+      })
+    }, 3000);
 
     var aLink = document.createElement("link");
     aLink.rel = "stylesheet";
     aLink.href =
       "https://cdn.jsdelivr.net/npm/addsearch-search-ui@0.4/dist/addsearch-search-ui.min.css";
     document.head.appendChild(aLink);
-   
+
   }, [])
 
 
   useEffect(() => {
-      var aScript = document.createElement("script");
-      aScript.type = "text/javascript";
-      aScript.src =
-        "/addsearch/js/ui.js";
-      document.body.appendChild(aScript);
-  },[]);
+    var aScript = document.createElement("script");
+    aScript.type = "text/javascript";
+    aScript.src =
+      "/addsearch/js/ui.js";
+    document.body.appendChild(aScript);
+  }, []);
 
   return (
     <Head>
@@ -129,7 +131,7 @@ const PageHead = ({ page }: { page: Type_Page }) => {
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
       <script></script>
     </Head>
-  );  
+  );
 };
 
 export default PageHead;
