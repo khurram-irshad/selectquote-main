295251676f6dc8093b76331524fa88d7d6b3a841
Rashid Aslam <rashid@outliant.com>
2023-03-16 16:26:58 +0500

    fixing scode

--- src/common/helpers/helper.ts
+++ src/common/helpers/helper.ts
@@ -47,3 +47,13 @@ export const isValidHttpUrl = (string) => {
   }
   return url.protocol === "http:" || url.protocol === "https:";
 };
+
+export const generateSessionId = () => {
+  var d = new Date().getTime();
+  var sessionID = "xxxxxx-xxxx-xxxxxx".replace(/[xy]/g, function (c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return sessionID;
+};
