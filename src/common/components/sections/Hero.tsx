505711b9f7223b6c093d1223cff263b39c57d309
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2023-03-18 11:45:20 +1300

    fix hero

--- src/common/components/sections/Hero.tsx
+++ src/common/components/sections/Hero.tsx
@@ -30,16 +30,38 @@ const HeroSection = ({ section }: { section: Type_Hero }) => {
   const [screenWidth, setScreenWidth] = useState(0);
 
   
- // path.includes('/newsroom') ? `50% 50%` : `right 0px ${backgroundPosition}`;
- let bgPosition = path.includes('customer-reviews') && screenWidth >= 1024 && screenWidth <= 1280 ? `right -150px ${backgroundPosition}` : backgroundPosition;
-  bgPosition = path.includes('leadership') && screenWidth >= 1024 && screenWidth <= 1280 ? `70% 0px !important` : bgPosition;
-  bgPosition = path.includes('/faqs') && screenWidth >= 1024 && screenWidth <= 1280 ? `70% 0px !important` : bgPosition;
-  bgPosition = path.includes('leadership') && screenWidth >= 580 && screenWidth <= 980 ? `center` : bgPosition;
+  // let bgPosition = path.includes('/newsroom') ? `50% 50%` : `right 0px ${backgroundPosition}`;
+  // bgPosition = path.includes('customer-reviews') && screenWidth >= 1024 && screenWidth <= 1280 ? `right -150px ${backgroundPosition}` : backgroundPosition;
+  // bgPosition = path.includes('leadership') && screenWidth >= 1024 && screenWidth <= 1280 ? `70% 0px !important` : bgPosition;
+  // bgPosition = path.includes('/faqs') && screenWidth >= 1024 && screenWidth <= 1280 ? `70% 0px !important` : bgPosition;
+  // bgPosition = path.includes('leadership') && screenWidth >= 580 && screenWidth <= 980 ? `center` : bgPosition;
 
-  let bgSize = path.includes('leadership') && screenWidth >= 580 && screenWidth <= 980 ? `cover` : mobileBackgroundSize;
+  let bgPosition = ``
+    //path === "/newsroom" ? `50% 50%` : `right 0px ${backgroundPosition}`;
+  
+  if (path === "/newsroom") {
+    bgPosition = `50% 50%`
+    console.log('news')
+  } else if (path.includes('customer-reviews') && screenWidth >= 1024 && screenWidth <= 1280) {
+    bgPosition = `right -150px ${backgroundPosition}`
+    console.log('customer')
+  } else if ((path.includes('faqs') || path.includes('leadership')) && screenWidth >= 1024 && screenWidth <= 1280) {
+    bgPosition = `70% 0px`
+    console.log('faqs')
+  } else if (path.includes('/leadership') && screenWidth >= 580 && screenWidth <= 980) {
+    bgPosition = `center`
+    console.log('leader')
+  } else { 
+    console.log('default')
+    bgPosition = `right 0px  ${backgroundPosition}`
+  }
 
   
-  
+  // console.log(bgPosition)
+  // bgPosition = path.includes('customer-reviews') && screenWidth >= 1024 && screenWidth <= 1280 ? `right -150px ${backgroundPosition}` : bgPosition;
+  // bgPosition = path.includes('/faqs') && screenWidth >= 1024 && screenWidth <= 1280 ? `70% 0px !important` : bgPosition;
+
+  const bgSize = path.includes('leadership') && screenWidth >= 580 && screenWidth <= 980 ? `cover` : mobileBackgroundSize;
   const bgImage = path.includes('customer-reviews') && screenWidth >= 1024 && screenWidth <= 1280 ? `linear-gradient(90deg,${gradientStartingColor} 33%,${gradientEndColor} 36%), url(https:${backgroundImage.fields.imageFile.fields.file.url})` : `linear-gradient(90deg,${gradientStartingColor} ${gradientStartingPercentage},${gradientEndColor} ${gradientEndPercentage}), url(https:${backgroundImage.fields.imageFile.fields.file.url})`
   
   
@@ -51,7 +73,7 @@ const HeroSection = ({ section }: { section: Type_Hero }) => {
       window.addEventListener("resize", handleResize);
       return () => window.removeEventListener("resize", handleResize);
     }, []);
-    console.log(bgPosition)
+   
   return (
     <div className="hero-block">
       <div className="hero-section-desktop ">
@@ -64,7 +86,7 @@ const HeroSection = ({ section }: { section: Type_Hero }) => {
             backgroundImage: backgroundImage
               ? `${bgImage}`
               : "",
-            padding,
+            //padding,
           }}
         >
           <div
@@ -99,7 +121,7 @@ const HeroSection = ({ section }: { section: Type_Hero }) => {
             style={{
               // backgroundColor: "#f8f8f8",
               backgroundSize: `${bgSize}`,
-              backgroundPosition: `${bgPosition}`,
+              backgroundPosition: `center top`,
               backgroundImage:
                 backgroundImageMobile && topSection
                   ? `linear-gradient(0deg,${gradientStartingColor} ${mobileGradientStartingPercent},${gradientEndColor} ${mobileGradientEndPercent}), url(https:${backgroundImageMobile.fields.imageFile.fields.file.url})`
