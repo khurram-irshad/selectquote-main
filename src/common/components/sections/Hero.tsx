ca7fc0520275a41de459c5f40747daa3de8aaa2b
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2023-04-01 01:11:26 +1300

    chore: fix hero for wide screens

--- src/common/components/sections/Hero.tsx
+++ src/common/components/sections/Hero.tsx
@@ -24,70 +24,136 @@ const HeroSection = ({ section }: { section: Type_Hero }) => {
     reverseImagePosition,
     mobileImageHeight,
     mobileImageWidth,
-    padding = '0',
+    padding = "0",
   } = section.fields;
   const path = useRouter().asPath;
   const [screenWidth, setScreenWidth] = useState(0);
 
-  
-  let bgPosition = ``
-  
+  let bgPosition = ``;
+
   if (path === "/newsroom") {
-    bgPosition = `50% 50%`
-  } else if (path.includes('customer-reviews') && screenWidth >= 1024 && screenWidth <= 1280) {
-    bgPosition = `right -150px ${backgroundPosition}`
-  } else if ((path.includes('faqs') || path.includes('leadership')) && screenWidth >= 1024 && screenWidth <= 1280) {
-    bgPosition = `70% 0px`
-  } else if (path.includes('leadership') && screenWidth >= 580 && screenWidth <= 980) {
-    bgPosition = `center`
-  } else if (path.includes('tv-commercial') && screenWidth >= 1024 && screenWidth <= 1280) {
-    bgPosition = '300%'
-  } else if (path === '/' || path.includes('/?')) { 
-    bgPosition = 'right'
-  } else{
-    bgPosition = `right 0px  ${backgroundPosition}`
+    bgPosition = `50% 50%`;
+  } else if (
+    path.includes("customer-reviews") &&
+    screenWidth >= 1024 &&
+    screenWidth <= 1280
+  ) {
+    bgPosition = `right -150px ${backgroundPosition}`;
+  } else if (
+    (path.includes("faqs") || path.includes("leadership")) &&
+    screenWidth >= 1024 &&
+    screenWidth <= 1280
+  ) {
+    bgPosition = `70% 0px`;
+  } else if (
+    path.includes("leadership") &&
+    screenWidth >= 580 &&
+    screenWidth <= 980
+  ) {
+    bgPosition = `center`;
+  } else if (
+    path.includes("tv-commercial") &&
+    screenWidth >= 1024 &&
+    screenWidth <= 1280
+  ) {
+    bgPosition = "300%";
+  } else if (path === "/" || path.includes("/?")) {
+    bgPosition = "right";
+  } else {
+    bgPosition = `right 0px  ${backgroundPosition}`;
   }
 
-  let bgSize = (path.includes('leadership') || path.includes('customer-review') || path.includes('/newsroom') || path.includes('/careers') || path.includes('contact')) && screenWidth >= 580 && screenWidth <= 980 ? `cover` : mobileBackgroundSize;
-  bgSize = path.includes('leadership') && screenWidth >= 752 && screenWidth <= 767 ? 'contain' : bgSize
-  let bgImage = path.includes('customer-reviews') && screenWidth >= 1024 && screenWidth <= 1280 ? `linear-gradient(90deg,${gradientStartingColor} 33%,${gradientEndColor} 36%), url(https:${backgroundImage.fields.imageFile.fields.file.url})` : `linear-gradient(90deg,${gradientStartingColor} ${gradientStartingPercentage},${gradientEndColor} ${gradientEndPercentage}), url(https:${backgroundImage.fields.imageFile.fields.file.url})`
-  bgImage = path.includes('rocket-lawyer') && screenWidth >= 1750 && screenWidth <= 2000 ? `linear-gradient(90deg,${gradientStartingColor} 58%,${gradientEndColor} 62%), url(https:${backgroundImage.fields.imageFile.fields.file.url})` : `linear-gradient(90deg,${gradientStartingColor} ${gradientStartingPercentage},${gradientEndColor} ${gradientEndPercentage}), url(https:${backgroundImage.fields.imageFile.fields.file.url})`
-  
-  let bgImageMobile = backgroundImageMobile && topSection && (path.includes('leadership') && screenWidth >= 768 && screenWidth <= 980 ? `linear-gradient(0deg,${gradientStartingColor} 37%,${gradientEndColor} 44%), url(https:${backgroundImageMobile.fields.imageFile.fields.file?.url})` : `linear-gradient(0deg,${gradientStartingColor} ${mobileGradientStartingPercent},${gradientEndColor} ${mobileGradientEndPercent}), url(https:${backgroundImageMobile.fields.imageFile.fields.file.url})`)
-  bgImageMobile = backgroundImageMobile && topSection && (path.includes('about') && screenWidth >= 580 && screenWidth <= 980  ? `linear-gradient(0deg,${gradientStartingColor} 33%,${gradientEndColor} 47%), url(https:${ backgroundImageMobile.fields.imageFile.fields.file?.url})` : `linear-gradient(0deg,${gradientStartingColor} ${mobileGradientStartingPercent},${gradientEndColor} ${mobileGradientEndPercent}), url(https:${backgroundImageMobile.fields.imageFile.fields.file.url})`)
-  
-  let bgPositionMobile = (path.includes('leadership') && screenWidth >= 768 && screenWidth <= 980) ? 'center' : 'center top'
-  
-  let mobilePaddingContent = topSection && backgroundImageMobile ? (path.includes('about') && screenWidth >= 580 && screenWidth <= 980 ? '400px' :  mobileContentPaddingTop) : 0
-  
-    useEffect(() => {
+  let bgSize =
+    (path.includes("leadership") ||
+      path.includes("customer-review") ||
+      path.includes("/newsroom") ||
+      path.includes("/careers") ||
+      path.includes("contact")) &&
+    screenWidth >= 580 &&
+    screenWidth <= 980
+      ? `cover`
+      : mobileBackgroundSize;
+  bgSize =
+    path.includes("leadership") && screenWidth >= 752 && screenWidth <= 767
+      ? "contain"
+      : bgSize;
+  let bgImage =
+    path.includes("customer-reviews") &&
+    screenWidth >= 1024 &&
+    screenWidth <= 1280
+      ? `linear-gradient(90deg,${gradientStartingColor} 33%,${gradientEndColor} 36%), url(https:${backgroundImage.fields.imageFile.fields.file.url})`
+      : `linear-gradient(90deg,${gradientStartingColor} ${gradientStartingPercentage},${gradientEndColor} ${gradientEndPercentage}), url(https:${backgroundImage.fields.imageFile.fields.file.url})`;
+  bgImage =
+    path.includes("rocket-lawyer") && screenWidth >= 1750 && screenWidth <= 2000
+      ? `linear-gradient(90deg,${gradientStartingColor} 58%,${gradientEndColor} 62%), url(https:${backgroundImage.fields.imageFile.fields.file.url})`
+      : `linear-gradient(90deg,${gradientStartingColor} ${gradientStartingPercentage},${gradientEndColor} ${gradientEndPercentage}), url(https:${backgroundImage.fields.imageFile.fields.file.url})`;
+
+  let bgImageMobile =
+    backgroundImageMobile &&
+    topSection &&
+    (path.includes("leadership") && screenWidth >= 768 && screenWidth <= 980
+      ? `linear-gradient(0deg,${gradientStartingColor} 37%,${gradientEndColor} 44%), url(https:${backgroundImageMobile.fields.imageFile.fields.file?.url})`
+      : `linear-gradient(0deg,${gradientStartingColor} ${mobileGradientStartingPercent},${gradientEndColor} ${mobileGradientEndPercent}), url(https:${backgroundImageMobile.fields.imageFile.fields.file.url})`);
+  bgImageMobile =
+    backgroundImageMobile &&
+    topSection &&
+    (path.includes("about") && screenWidth >= 580 && screenWidth <= 980
+      ? `linear-gradient(0deg,${gradientStartingColor} 33%,${gradientEndColor} 47%), url(https:${backgroundImageMobile.fields.imageFile.fields.file?.url})`
+      : `linear-gradient(0deg,${gradientStartingColor} ${mobileGradientStartingPercent},${gradientEndColor} ${mobileGradientEndPercent}), url(https:${backgroundImageMobile.fields.imageFile.fields.file.url})`);
+
+  let bgPositionMobile =
+    path.includes("leadership") && screenWidth >= 768 && screenWidth <= 980
+      ? "center"
+      : "center top";
+
+  let mobilePaddingContent =
+    topSection && backgroundImageMobile
+      ? path.includes("about") && screenWidth >= 580 && screenWidth <= 980
+        ? "400px"
+        : mobileContentPaddingTop
+      : 0;
+
+  useEffect(() => {
+    const hero = document.getElementById("heroSection");
+    const heroContent = document.getElementById("heroContent");
+    hero.classList.remove("container");
+    heroContent.classList.add("container");
+    if (screenWidth >= 1620 && path.includes("auto-and-home-insurance")) {
+      hero.classList.add("container");
+      hero.classList.remove("hero-section-shadow");
+      heroContent.classList.remove("container");
+    }
+  }, [screenWidth]);
+
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+    function handleResize() {
       setScreenWidth(window.innerWidth);
-      function handleResize() {
-        setScreenWidth(window.innerWidth);
-      }
-      window.addEventListener("resize", handleResize);
-      return () => window.removeEventListener("resize", handleResize);
-    }, []);
-   
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="hero-block">
       <div className="hero-section-desktop ">
         <section
-          className={`hero-section ${topSection ? "  hero-section-shadow" : ""
-            } ${!topSection ? 'container' : ''}`}
+          className={`hero-section ${
+            topSection ? "  hero-section-shadow" : ""
+          } ${!topSection ? "container" : ""}`}
           style={{
             backgroundSize: `${backgroundSize}`,
             backgroundPosition: `${bgPosition}`,
-            backgroundImage: backgroundImage
-              ? `${bgImage}`
-              : "",
+            backgroundImage: backgroundImage ? `${bgImage}` : "",
             //padding,
           }}
+          id="heroSection"
         >
           <div
-            className={
-              `hero-content d-flex align-items-center  wp-container ${topSection ? 'container' : ''}`
-            }
+            className={`hero-content d-flex align-items-center  wp-container ${
+              topSection ? "container" : ""
+            }`}
+            id="heroContent"
           >
             <div className="w-50 position-relative">
               <div className="content-left">
@@ -95,8 +161,8 @@ const HeroSection = ({ section }: { section: Type_Hero }) => {
                   <div key={item.sys.id} className={`d-flex`}>
                     <div key={section.sys.id} style={{ width: "100%" }}>
                       {item.sys.contentType?.sys.id ===
-                        ComponentContentTypes.MultiColumn ? (
-                        <MultiColumnSection section={item}  />
+                      ComponentContentTypes.MultiColumn ? (
+                        <MultiColumnSection section={item} />
                       ) : (
                         <ColumnSection section={item} />
                       )}
@@ -112,12 +178,14 @@ const HeroSection = ({ section }: { section: Type_Hero }) => {
       <div className="hero-section-mobile">
         {!reverseImagePosition && (
           <section
-            className={`hero-section ${path.includes('/faqs') ? ' spacing-top' : ''}`}
+            className={`hero-section ${
+              path.includes("/faqs") ? " spacing-top" : ""
+            }`}
             style={{
               // backgroundColor: "#f8f8f8",
               backgroundSize: `${bgSize}`,
               backgroundPosition: bgPositionMobile,
-              backgroundImage: bgImageMobile || ''
+              backgroundImage: bgImageMobile || "",
             }}
           >
             <div
@@ -132,7 +200,7 @@ const HeroSection = ({ section }: { section: Type_Hero }) => {
                     <div key={item.sys.id} className={`d-flex`}>
                       <div key={section.sys.id} style={{ width: "100%" }}>
                         {item.sys.contentType?.sys.id ===
-                          ComponentContentTypes.MultiColumn ? (
+                        ComponentContentTypes.MultiColumn ? (
                           <MultiColumnSection section={item} />
                         ) : (
                           <div>
@@ -159,7 +227,7 @@ const HeroSection = ({ section }: { section: Type_Hero }) => {
                     <div key={item.sys.id} className={`d-flex`}>
                       <div key={section.sys.id} style={{ width: "100%" }}>
                         {item.sys.contentType?.sys.id ===
-                          ComponentContentTypes.MultiColumn ? (
+                        ComponentContentTypes.MultiColumn ? (
                           <MultiColumnSection section={item} />
                         ) : (
                           <div>
