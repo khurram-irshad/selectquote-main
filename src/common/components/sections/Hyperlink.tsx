6178d434e7a20cff5c4d4249dd8a15a35084eceb
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2023-04-05 00:31:04 +1200

    revert Hyperlink

--- src/common/components/sections/Hyperlink.tsx
+++ src/common/components/sections/Hyperlink.tsx
@@ -5,10 +5,10 @@ import React from "react";
 const Hyperlink = ({ section }: { section: Type_HyperLink }) => {
   const { type, underline, externalLink } = section.fields;
   const title = section.fields.title;
-  let titleWithSuperscript = '';
+  let titleWithSuperscript = "";
   let currentIndex = 0;
   while (true) {
-    const superscriptIndex = title.indexOf('®', currentIndex);
+    const superscriptIndex = title.indexOf("®", currentIndex);
     if (superscriptIndex === -1) {
       titleWithSuperscript += title.substring(currentIndex);
       break;
@@ -17,21 +17,28 @@ const Hyperlink = ({ section }: { section: Type_HyperLink }) => {
     titleWithSuperscript += `<sup>®</sup>`;
     currentIndex = superscriptIndex + 1;
   }
-  
-  return <Link href={section.fields.scrollToId ?? (section.fields.linkUrl || "/")} legacyBehavior scroll={false}>
-    <a
-      className={type === 'Button' ? 'link-button hyperlink' : 'hyperlink'}
-      target={externalLink ? '_blank' : ''}
-      rel={externalLink ? 'noopener noreferrer' : ''}
-      style={{
-        color: section.fields?.color,
-        textDecoration: !underline ? "none" : 'underline',
-        fontSize: section.fields.fontSize,
-        fontWeight: section.fields.fontWeight,
-      }}
+
+  return (
+    <Link
+      href={section.fields.scrollToId ?? (section.fields.linkUrl || "/")}
+      legacyBehavior
+      scroll={false}
     >
-       <span dangerouslySetInnerHTML={{ __html: titleWithSuperscript }} />
-    </a>
-</Link>
+      <a
+        className={type === "Button" ? "link-button hyperlink" : "hyperlink"}
+        target={externalLink ? "_blank" : ""}
+        rel={externalLink ? "noopener noreferrer" : ""}
+        style={{
+          color: section.fields?.color,
+          textDecoration: !underline ? "none" : "underline",
+          fontSize: section.fields.fontSize,
+          fontWeight: section.fields.fontWeight,
+        }}
+      >
+        <span dangerouslySetInnerHTML={{ __html: titleWithSuperscript }} />
+      </a>
+    </Link>
+  );
+};
 
 export default Hyperlink;
