9b8db05322cddfd42e8a71510202002068487559
Rashid Aslam <rashid@outliant.com>
2023-04-07 15:17:49 +0500

    tooltip issue fixed

--- src/common/components/sections/Image.tsx
+++ src/common/components/sections/Image.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Type_Image } from "@common/types";
-import { DeviceType } from "@common/types/Type_Device";
+import Device, { DeviceType } from "@common/types/Type_Device";
 import { useGlobalContext } from "src/context";
 import { isDesktop, isMobile } from "@common/helpers/helper";
 import { Tooltip } from "react-tooltip";
@@ -9,11 +9,7 @@ import RichTextRenderer from "@components/rich-text/RichTextRenderer";
 
 const ImageSection = ({ section }: { section: Type_Image }) => {
   const {
-    imageFile,
-    imageName,
     externalLink,
-    fill,
-    quality,
     link,
     tooltip,
     tooltipId,
@@ -26,99 +22,26 @@ const ImageSection = ({ section }: { section: Type_Image }) => {
   );
 
   const { screenMode } = useGlobalContext();
-  const widthDesktop: any = desktop?.fields?.width
-    ? desktop.fields.width.replace("px", "")
-    : imageFile?.fields?.file?.details?.image?.width;
-  const heightDesktop: any = desktop?.fields?.height
-    ? desktop.fields.height.replace("px", "")
-    : imageFile?.fields?.file?.details?.image?.height;
-
-  const widthMobile: any = mobile?.fields?.width
-    ? mobile.fields.width.replace("px", "")
-    : imageFile?.fields?.file?.details?.image?.width;
-  const heightMobile: any = mobile?.fields?.height
-    ? mobile.fields.height.replace("px", "")
-    : imageFile?.fields?.file?.details?.image?.height;
-
   const renderImage = () => {
     return (
       <>
         {isDesktop(screenMode) &&
           (tooltipId ? (
             <a data-tooltip-id={tooltipId}>
-              <span className={"image-container"}>
-                {fill ? (
-                  <Image
-                    quality={100}
-                    style={{ borderRadius: desktop?.fields?.borderRadius }}
-                    src={`https:${imageFile?.fields?.file?.url}`}
-                    fill
-                    className={"image h-unset"}
-                    alt={imageName || imageFile?.fields?.title}
-                  />
-                ) : (
-                  <Image
-                    quality={100}
-                    style={{ borderRadius: desktop?.fields?.borderRadius }}
-                    src={`https:${imageFile?.fields?.file?.url}`}
-                    width={Number(widthDesktop)}
-                    height={Number(heightDesktop)}
-                    className={"image "}
-                    alt={imageName || imageFile?.fields?.title}
-                  />
-                )}
-              </span>
+              <GetImage device={desktop?.fields} section={section} />
             </a>
           ) : (
-            <span className={"image-container"}>
-              {fill ? (
-                <Image
-                  quality={100}
-                  style={{ borderRadius: desktop?.fields?.borderRadius }}
-                  src={`https:${imageFile?.fields?.file?.url}`}
-                  fill
-                  className={"image h-unset"}
-                  alt={imageName || imageFile?.fields?.title}
-                />
-              ) : (
-                <Image
-                  quality={100}
-                  style={{ borderRadius: desktop?.fields?.borderRadius }}
-                  src={`https:${imageFile?.fields?.file?.url}`}
-                  width={Number(widthDesktop)}
-                  height={Number(heightDesktop)}
-                  className={"image "}
-                  alt={imageName || imageFile?.fields?.title}
-                />
-              )}
-            </span>
+            <GetImage device={desktop?.fields} section={section} />
           ))}
         {isMobile(screenMode) && (
-          <span
-            className={"image-container"}
-            style={{ width: mobile?.fields.widthPercentage + "%" }}
-          >
-            {fill ? (
-              <Image
-                quality={100}
-                style={{ borderRadius: mobile?.fields?.borderRadius }}
-                src={`https:${imageFile?.fields?.file?.url}`}
-                fill
-                className={"image h-unset"}
-                alt={imageName || imageFile?.fields?.title}
-              />
-            ) : (
-              <Image
-                quality={100}
-                style={{ borderRadius: mobile?.fields?.borderRadius }}
-                src={`https:${imageFile?.fields?.file?.url}`}
-                width={Number(widthMobile)}
-                height={Number(heightMobile)}
-                className={"image "}
-                alt={imageName || imageFile?.fields?.title}
-              />
-            )}
-          </span>
+          (tooltipId ? (
+            <a data-tooltip-id={tooltipId}>
+              <GetImage device={mobile?.fields} section={section} />
+            </a>
+          ) : (
+            <GetImage device={mobile?.fields} section={section} />
+          ))
+
         )}
         {tooltipId && (
           <Tooltip id={tooltipId} className="tooltip" openOnClick clickable>
@@ -146,4 +69,44 @@ const ImageSection = ({ section }: { section: Type_Image }) => {
   );
 };
 
+const GetImage = ({ section, device }: { section: Type_Image, device: Device }) => {
+  const {
+    imageFile,
+    imageName,
+    fill,
+  } = section.fields;
+
+  const width: any = device?.width
+    ? device?.width.replace("px", "")
+    : imageFile?.fields?.file?.details?.image?.width;
+  const height: any = device?.height
+    ? device?.height.replace("px", "")
+    : imageFile?.fields?.file?.details?.image?.height;
+
+  return <span
+    className={"image-container"}
+    style={{ width: device?.widthPercentage + "%" }}
+  >
+    {fill ? (
+      <Image
+        quality={100}
+        style={{ borderRadius: device?.borderRadius }}
+        src={`https:${imageFile?.fields?.file?.url}`}
+        fill
+        className={"image h-unset"}
+        alt={imageName || imageFile?.fields?.title}
+      />
+    ) : (
+      <Image
+        quality={100}
+        style={{ borderRadius: device?.borderRadius }}
+        src={`https:${imageFile?.fields?.file?.url}`}
+        width={Number(width)}
+        height={Number(height)}
+        className={"image "}
+        alt={imageName || imageFile?.fields?.title}
+      />
+    )}
+  </span>
+}
 export default ImageSection;
